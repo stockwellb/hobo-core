d97275ed19ece0dfdbb48e9d376b6bfd445b29c5
Bruce Stockwell <bruce.stockwell@gmail.com>
2026-06-13 16:46:53 -0400

    added some more checks

--- include/hobo/check.h
+++ include/hobo/check.h
@@ -1,21 +1,45 @@
 #ifndef HOBO_CHECK_H
 #define HOBO_CHECK_H
+#include <stdbool.h>
 #include <stdio.h>
 
 extern int _hobo_check_failed;
 
+void test_check(bool passed, const char *expr, const char *file, int line);
+void test_check_eq_int(int actual, int expected, const char *actual_expr,
+                       const char *expected_expr, const char *file, int line);
+
+void test_check_ne_int(int actual, int expected, const char *actual_expr,
+                       const char *expected_expr, const char *file, int line);
+
+void test_check_eq_float(float actual, float expected, float epsilon,
+                         const char *actual_expr, const char *expected_expr,
+                         const char *file, int line);
+
+void test_check_null(const void *ptr, const char *ptr_expr, const char *file,
+                     int line);
+
+void test_check_not_null(const void *ptr, const char *ptr_expr,
+                         const char *file, int line);
+
 #define HOBO_CHECK_RESET() (_hobo_check_failed = 0)
 #define HOBO_CHECK_RESULT() (_hobo_check_failed ? TEST_FAIL : TEST_PASS)
+#define CHECK(cond) test_check(!!(cond), #cond, __FILE__, __LINE__)
+#define CHECK_TRUE(cond)                                                       \
+  test_check(!!(cond), #cond " is true", __FILE__, __LINE__)
 
-#define CHECK(cond)                                                            \
-  do {                                                                         \
-    if (!(cond)) {                                                             \
-      _hobo_check_failed = 1;                                                  \
-      printf("  [FAIL] %s\n", #cond);                                          \
-      printf("         at %s:%d\n", __FILE__, __LINE__);                       \
-    } else {                                                                   \
-      printf("  [PASS] %s\n", #cond);                                          \
-    }                                                                          \
-  } while (0)
+#define CHECK_FALSE(cond)                                                      \
+  test_check(!(cond), #cond " is false", __FILE__, __LINE__)
+
+#define CHECK_EQ_INT(actual, expected)                                         \
+  test_check_eq_int((actual), (expected), #actual, #expected, __FILE__,        \
+                    __LINE__)
+
+#define CHECK_NE_INT(actual, expected)                                         \
+  test_check_ne_int((actual), (expected), #actual, #expected, __FILE__,        \
+                    __LINE__)
+
+#define CHECK_NULL(ptr) test_check_null((ptr), #ptr, __FILE__, __LINE__)
+#define CHECK_NOT_NULL(ptr) test_check_not_null((ptr), #ptr, __FILE__, __LINE__)
 
 #endif
--- src/check.c
+++ src/check.c
@@ -1,3 +1,61 @@
 #include "hobo/check.h"
 
 int _hobo_check_failed = 0;
+
+static void test_pass(const char *msg) { printf("  [PASS] %s\n", msg); }
+
+static void test_fail(const char *msg, const char *file, int line) {
+  _hobo_check_failed = 1;
+  printf("  [FAIL] %s\n", msg);
+  printf("         at %s:%d\n", file, line);
+}
+
+void test_check(bool passed, const char *expr, const char *file, int line) {
+  if (passed) {
+    test_pass(expr);
+  } else {
+    test_fail(expr, file, line);
+  }
+}
+
+void test_check_eq_int(int actual, int expected, const char *actual_expr,
+                       const char *expected_expr, const char *file, int line) {
+  if (actual == expected) {
+    test_pass("ok");
+  } else {
+    test_fail("integer equality failed", file, line);
+    printf("         actual:   %s = %d\n", actual_expr, actual);
+    printf("         expected: %s = %d\n", expected_expr, expected);
+  }
+}
+
+void test_check_ne_int(int actual, int expected, const char *actual_expr,
+                       const char *expected_expr, const char *file, int line) {
+  if (actual != expected) {
+    test_pass("ok");
+  } else {
+    test_fail("integer inequality failed", file, line);
+    printf("         actual:   %s = %d\n", actual_expr, actual);
+    printf("         expected: %s = %d\n", expected_expr, expected);
+  }
+}
+
+void test_check_not_null(const void *ptr, const char *ptr_expr,
+                         const char *file, int line) {
+  if (ptr != NULL) {
+    test_pass(ptr_expr);
+  } else {
+    test_fail("expected non-null pointer", file, line);
+    printf("         actual:   %s = NULL\n", ptr_expr);
+  }
+}
+
+void test_check_null(const void *ptr, const char *ptr_expr, const char *file,
+                     int line) {
+  if (ptr == NULL) {
+    test_pass(ptr_expr);
+  } else {
+    test_fail("expected null pointer", file, line);
+    printf("         actual:   %s = %p\n", ptr_expr, ptr);
+  }
+}
